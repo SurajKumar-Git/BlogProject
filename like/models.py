6e9a5b2e827017494401d25c3deb28a94eb71a6d
Suraj Kumar <surajk998088@gmail.com>
2020-06-26 22:27:26 +0530

    Added Like Model in Like App

--- like/models.py
+++ like/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from blog.models import Post
+from django.conf import settings
 # Create your models here.
+
+
+class Like(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
