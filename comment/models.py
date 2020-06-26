70842c5c661d5e4b8fb513c319451a8467495fca
Suraj Kumar <surajk998088@gmail.com>
2020-06-26 22:29:34 +0530

    Added Comment Model in Comment App

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.conf import settings
+from blog.models import Post
 # Create your models here.
+
+
+class Comment(models.Model):
+
+    message = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.message
