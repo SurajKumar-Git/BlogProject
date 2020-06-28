95adba22b240172fa12842d9d82367129399e418
Suraj Kumar <surajk998088@gmail.com>
2020-06-28 10:55:12 +0530

    Added Register Form

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,22 @@
-from django.contrib.auth import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+from django import forms
 
 
-class LoginForm(forms.AuthenticationForm):
+class LoginForm(AuthenticationForm):
     pass
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, widget=forms.TextInput())
+    last_name = forms.CharField(max_length=150, widget=forms.TextInput())
+    email = forms.EmailField(widget=forms.EmailInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = ["first_name", "last_name", "username",
+                  "email", "password1", "password2"]
+        help_texts = {
+            'username': 'Letters, digits and @/./+/-/_ only.',
+            'password2': 'Enter the same password as before, for verification.',
+        }
