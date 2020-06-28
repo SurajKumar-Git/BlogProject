7fbcdff8a6adcc83e54bc644971e298ba6a17b73
Suraj Kumar <surajk998088@gmail.com>
2020-06-28 22:10:32 +0530

    Added Like and Comment apps to Installed App

--- BlogProject/urls.py
+++ BlogProject/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
     path("account/", include("account.urls")),
+    path("like/", include("like.urls")),
+    path("comment/", include("comment.urls")),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
