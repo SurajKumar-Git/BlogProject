dac4f927023f778e90884462fb5d52b3db753a5c
Suraj Kumar <surajk998088@gmail.com>
2020-06-29 19:59:47 +0530

    Added Comment create, edit and delete views and urls

--- comment/urls.py
+++ comment/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from comment.views import post_comments
+from comment.views import PostCommentCreateView, PostCommentUpdateView, PostCommentDeleteView
 
 app_name = "comment"
 urlpatterns = [
-    path("<int:post_id>/", post_comments, name="post_comments")
+    path("post/<int:post_id>/", PostCommentCreateView.as_view(), name="post_comments"),
+    path("<int:pk>/edit",
+         PostCommentUpdateView.as_view(), name="comment_edit"),
+    path("<int:pk>/delete", PostCommentDeleteView.as_view(), name="comment_delete")
+
 ]
--- comment/views.py
+++ comment/views.py
@@ -1,32 +1,68 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from comment.models import Comment
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView, UpdateView, DeleteView
 from comment.forms import CommentForm
 from django.template.loader import render_to_string
 from django.http import JsonResponse
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
 # Create your views here.
 
 
-def post_comments(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+class PostCommentCreateView(CreateView):
+    model = Comment
+    fields = ['message']
 
-    if request.method == "POST":
-        comment_form = CommentForm(request.POST)
-        comment_form.instance.set_post(post)
-        comment_form.instance.set_user(request.user)
-        comment_form.save()
-    else:
-        comment_form = CommentForm()
-    comments = post.get_post_comments().order_by('-date_modified')
-    comments_count = post.get_post_comments_count()
-    context = {
-        'post': post,
-        "comments": comments
-    }
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
 
-    if request.is_ajax():
-        form_html = render_to_string("comment/comments.html",
-                                     context, request=request)
-        return JsonResponse({"form": form_html, 'comments_count': comments_count})
+    def form_valid(self, form):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        form.instance.set_post(post)
+        form.instance.set_user(self.request.user)
+        self.object = form.save()
+        comments_count = post.get_post_comments_count()
+        if self.request.is_ajax():
+            context = {'comment': self.object}
+            comment_html = render_to_string("comment/comment.html",
+                                            context, request=self.request)
+            return JsonResponse({"comment_html": comment_html, "comments_count": comments_count}, status=200)
+
+
+class PostCommentUpdateView(UpdateView):
+    model = Comment
+    fields = ['message']
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data()
+        comment_form_html = render_to_string("comment/comment_form.html",
+                                             context, request=self.request)
+        return JsonResponse({"comment_form_html": comment_form_html}, status=200)
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+
+    def form_valid(self, form):
+        self.object = form.save()
+        if self.request.is_ajax():
+            context = {'comment': self.object}
+            comment_html = render_to_string("comment/update_comment.html",
+                                            context, request=self.request)
+            return JsonResponse({"comment_html": comment_html}, status=200)
+
+
+class PostCommentDeleteView(DeleteView):
+    model = Comment
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        post = self.object.get_post()
+        self.object.delete()
+        comments_count = post.get_post_comments_count()
+        if self.request.is_ajax():
+            return JsonResponse({"comments_count": comments_count}, status=200)
