3ec01076d96a202e61cfc4038640e2f08eb44997
Suraj Kumar <surajk998088@gmail.com>
2020-06-30 17:31:58 +0530

    Update urls - added / at the end of urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,15 +4,15 @@ from blog.views import SearchBlogs
 app_name = "blog"
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
-    path("category/<slug:slug>", BlogFilterByCategory.as_view(),
+    path("category/<slug:slug>/", BlogFilterByCategory.as_view(),
          name="category_filter"),
-    path("blog/add", AddBlogPost.as_view(), name="add_blog_post"),
-    path("blog/search", SearchBlogs.as_view(), name="blog_search"),
-    path("blog/<slug:slug>", BlogDetailView.as_view(), name="blog_detail"),
-    path("blog/<slug:slug>/update",
+    path("blog/add/", AddBlogPost.as_view(), name="add_blog_post"),
+    path("blog/search/", SearchBlogs.as_view(), name="blog_search"),
+    path("blog/<slug:slug>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("blog/<slug:slug>/update/",
          UpdateBlogPost.as_view(), name="update_blog_post"),
-    path("blog/<slug:slug>/delete",
+    path("blog/<slug:slug>/delete/",
          DeleteBlogPost.as_view(), name="delete_blog_post"),
-    path("blog/<slug:slug>/view", AuthorBlogsDetailView.as_view(),
+    path("blog/<slug:slug>/view/", AuthorBlogsDetailView.as_view(),
          name="view_author_blog_detail"),
 ]
--- comment/urls.py
+++ comment/urls.py
@@ -4,8 +4,8 @@ from comment.views import PostCommentCreateView, PostCommentUpdateView, PostComm
 app_name = "comment"
 urlpatterns = [
     path("post/<int:post_id>/", PostCommentCreateView.as_view(), name="post_comments"),
-    path("<int:pk>/edit",
+    path("<int:pk>/edit/",
          PostCommentUpdateView.as_view(), name="comment_edit"),
-    path("<int:pk>/delete", PostCommentDeleteView.as_view(), name="comment_delete")
+    path("<int:pk>/delete/", PostCommentDeleteView.as_view(), name="comment_delete")
 
 ]
